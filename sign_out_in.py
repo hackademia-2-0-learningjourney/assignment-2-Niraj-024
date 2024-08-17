3ffdb709326839bc2050b81d394544490d867cac
Niroj Gamal <lotus06022@gmail.com>
2024-08-17 20:32:02 +0545

    Add files via upload
    error solved here

--- sign_out_in.py
+++ sign_out_in.py
@@ -5,11 +5,11 @@ def load_user():
     try: 
         with open("db_user.json","r") as f:
             return json.load(f)
-    except Exception as e: 
-        print("Error occured, try again")
+    except Exception as e : 
+        return{}
 
 #save users to file
-def save_user():
+def save_user(users):
     with open ("db_user.json", "w") as f:
         json.dump(users, f, indent = 3)
 
@@ -37,20 +37,25 @@ while True:
 
         save_user(users)
         print('Succesful Sign up')
+        print(f'***'*25)
 
     elif choice == '2' :
         u_name= input("Enter username:")
         password= input("Enter password:")
 
         if u_name in users and users[u_name]['password']== password:
-            print("login successful.. Your contact number is:: {users[u_name]['ph_no]}")
+            print(f"login successful.. Your contact number is:: {users[u_name]['ph_no']}")
+            print(f'***'*25)
+
         else :
             print ("Error, try again")
+            print(f'***'*25)
+
             break
 
     else:
         print("Choose either 1 or 2...")
-
+        print(f'***'*25)
 
 
 
